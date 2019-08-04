2ed9c16b7aa7e5fa374f353b048900a2fc6390b6
silenced sre <skr3sna@gmail.com>
2019-08-04 18:25:43 +0545

    post fixed

--- src/App.js
+++ src/App.js
@@ -1,83 +1,72 @@
-    import React, { Component } from 'react';
-    import CKEditor from '@ckeditor/ckeditor5-react';
-    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import React, { Component } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-    import axios from 'axios';
+import axios from 'axios';
 
-    class App extends Component {
+class App extends Component {
 
-        state = {
-            contents: []
-        }
+    state = {
+        contents: [],
+        post: ``
+    };
 
-        componentDidMount() {
-            axios.get('https://ckeditor-react-django.herokuapp.com/post/',
-                {headers: {
+    onButtonSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.post);
+        axios.defaults.xsrfHeaderName = "X-CSRFToken";
+        axios.post('https://ckeditor-react-django.herokuapp.com/post/',
+            {content: this.state.post},
+            {headers:
+                    {'Authorization': 'Token 9fed17335075119580fce6fb341e7c476dfcbc12'}
+            });
+    };
+
+    componentDidMount() {
+        axios.get('https://ckeditor-react-django.herokuapp.com/post/',
+            {headers: {
                     'Authorization': 'Token 9fed17335075119580fce6fb341e7c476dfcbc12'
                 }
-                })
-                .then(response => {
-                    console.log(response);
-                    this.setState({
-                        contents: response.data
-                    })
-                });
-        }
-
-        createMarkup = (markup) => {
-            return{__html:markup}
-        }
-
-
-        render(){
-
-            const ckeditorList = this.state.contents.map(content => {
-                return (
-                    <div key={content.id}>
-                        <div dangerouslySetInnerHTML={this.createMarkup(content.content)}></div>
-                        <hr />
-                    </div>
-                )
             })
-
-            return (
-                <div className="App">
-                    {ckeditorList}
-                    <h2>Hello from CKeditor</h2>
-
-
-
-                    <CKEditor
-                        editor={ ClassicEditor }
-                        data="<p>Hello from CKEditor 5!</p>"
-                        onInit={ editor => {
-                            // You can store the "editor" and use when it is needed.
-                            console.log( 'Editor is ready to use!', editor );
-                        } }
-                        onChange={ ( event, editor ) => {
-                            const data = editor.getData();
-                            console.log( { event, editor, data } );
-                            axios.defaults.xsrfHeaderName = "X-CSRFToken";
-                            axios.post('https://ckeditor-react-django.herokuapp.com/post/',
-                                {
-                                content: data
-                            },
-                                {
-                                    headers: {
-                                        'Authorization': 'Token 9fed17335075119580fce6fb341e7c476dfcbc12'
-                                    }
-                                })
-                        } }
-                        onBlur={ editor => {
-                            console.log( 'Blur.', editor );
-                        } }
-                        onFocus={ editor => {
-                            console.log( 'Focus.', editor );
-                        } }
-                    />
-                </div>
-            );
-        }
+            .then(response => {
+                console.log(response);
+                this.setState({
+                    contents: response.data
+                })
+            });
     }
 
-    export default App;
+    createMarkup = (markup) => {
+        return{__html:markup}
+    }
+
+
+    render(){
+
+        const ckeditorList = this.state.contents.map(content => {
+            return (
+                <div key={content.id}>
+                    <div dangerouslySetInnerHTML={this.createMarkup(content.content)}></div>
+                    <hr />
+                </div>
+            )
+        })
+
+        return (
+            <div className="App">
+                {ckeditorList}
+                <h2>Hello from CKeditor</h2>
+
+                <CKEditor
+                    editor={ ClassicEditor }
+                    onChange={ ( event, editor ) => {
+                        this.setState({post: editor.getData()});
+                    }}
+                />
+                <button onClick={this.onButtonSubmit}>Submit</button>
+            </div>
+        );
+    }
+}
+
+export default App;
